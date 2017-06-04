06f87a7d889dfb7846cfd0a5e2ab56d16fbbbd6e
Nabil Muhammad Firdaus <nabilftd@gmail.com>
2017-06-05 05:07:49 +0700

    Changed title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel 5.5
                 </div>
 
                 <div class="links">
